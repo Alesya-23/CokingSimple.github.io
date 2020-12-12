1bdd1325496714a6bce8154b7353051631698f7a
aleca <aleca2002@yandex.ru>
2020-12-13 00:27:20 +0300

    Commin1.3

--- site.js
+++ site.js
@@ -1,1 +1,37 @@
-alert('Hi!')
+const form = document.forms[0];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  new FormData(form);
+});
+
+form.addEventListener("formdata", event => {
+  const data = event.formData;
+
+  // get the data
+  const entries = [...data.entries()];
+  console.log(entries);
+
+  const values = [...data.values()];
+  console.log(values);
+
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+  })
+  
+  Swal.fire({
+    title: 'Ты закончил создание рецепта и хочешь его добавить?',
+    showCancelButton: true,
+    confirmButtonText: `Добавить`,
+    CancelButtonText: `Закрыть и продолжить редактирование`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Рецепт добавлен!', '', 'success')
+    }
+  })
+});
