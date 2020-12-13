d6cba57c12307649a7fe185387656aa073de1425
aleca <aleca2002@yandex.ru>
2020-12-13 16:41:49 +0300

    commit1.14

--- site.js
+++ site.js
@@ -1,15 +1,11 @@
 let formData = new FormData();
-const form = document.forms[0].elements[9];
-form.addEventListener("submit", buttonPressed);
+var buttonsubmit = document.forms.recheptsform;
+buttonsubmit.addEventListener("click", buttonPressed);
 console.log("Скрипт загружен")
-var event = document.getElementById('bthaddrechept');
-function buttonPressed(event) {
-  if(event = null){
-    return; 
-  }
-  console.log('Button is pressed')
-  event.preventDefault();
-  if (document.forms[0].elements[0].value = '') {
+function buttonPressed(evt) {
+  console.log("Кнопка нажата загружен");
+  evt.preventDefault();
+  if (document.forms.recheptsform.elements.namerecept.value == '') {
     Swal.fire(
       'Ошибка!',
       'Вы не ввели название рецепта!',
@@ -17,63 +13,63 @@ function buttonPressed(event) {
     )
     console.log("Нет названия рецепта");
   }
-  else if (document.forms[0].elements[1].value = '') {
+  else if (document.forms.recheptsform.elements.portion.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели количество порций',
       'error'
     )
   }
-  else if (document.forms[0].elements[2].value = '') {
+  else if (document.forms.recheptsform.elements.timecooking.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели время приготовления',
       'error'
     )
   }
-  else if (document.forms[0].elements[3].value = '') {
+  else if (document.forms.recheptsform.elements.ingredient1.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели ингредиент',
       'error'
     )
   }
-  else if (document.forms[0].elements[4].value = '') {
+  else if (document.forms.recheptsform.elements.count1.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели количество ингредиента',
       'error'
     )
   }
-  else if (document.forms[0].elements[5].value = '') {
+  else if (document.forms.recheptsform.elements.ingredient2.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели ингредиент',
       'error'
     )
   }
-  else if (document.forms[0].elements[6].value = '') {
+  else if (document.forms.recheptsform.elements.count2.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели количество ингредиента',
       'error'
     )
   }
-  else if (document.forms[0].elements[7].value = '') {
+  else if (document.forms.recheptsform.elements.step1.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели шаг 1',
       'error'
     )
   }
-  else if (document.forms[0].elements[8].value = '') {
+  else if (document.forms.recheptsform.elements.step2.value == '') {
     Swal.fire(
       'Ошибка',
       'Вы не ввели шаг 2',
       'error'
     )
   }
-  else if (document.forms[0].elements[9].value = 'Выберите категорию блюда') {
+  else if (document.forms.recheptsform.elements.spinnertypecook.value == 'Выберите категорию блюда') {
     Swal.fire(
       'Ошибка',
       'Вы не выбрали категорию блюда',
@@ -81,22 +77,23 @@ function buttonPressed(event) {
     )
   }
   else {
-    formData.append('namerechepts', document.forms[0].elements[0].value);
-    formData.append('portion', document.forms[0].elements[1].value);
-    formData.append('timecook', document.forms[0].elements[2].value);
-    formData.append('ingridientone', document.forms[0].elements[3].value);
-    formData.append('countingridientone', document.forms[0].elements[4].value);
-    formData.append('ingridienttwo', document.forms[0].elements[5].value);
-    formData.append('countingridienttwo', document.forms[0].elements[6].value);
-    formData.append('stepone', document.forms[0].elements[7].value);
-    formData.append('steptwo', document.forms[0].elements[8].value);
-    formData.append('typecook', document.forms[0].elements[9].value);
+    formData.append('namerecept', document.forms.recheptsform.elements.namerecept.value);
+    formData.append('portion', document.forms.recheptsform.elements.portion.value);
+    formData.append('timecook', document.forms.recheptsform.elements.timecooking.value);
+    formData.append('ingridientone', document.fors.recheptsform.elements.ingredient1.value);
+    formData.append('countingridientone', document.forms.recheptsform.elements.count1.value);
+    formData.append('ingridienttwo', document.forms.recheptsform.elements.ingredient2.value);
+    formData.append('countingridienttwo', document.forms.recheptsform.elements.count2.value);
+    formData.append('stepone', document.forms.recheptsform.elements.step1.value);
+    formData.append('steptwo', document.forms.recheptsform.elements.step2.value);
+    formData.append('typecook', document.forms.recheptsform.elements.spinnertypecook.value);
     Swal.fire(
-      'Вы добавили рецепт' + formData.get('namerechepts') +
+      'Добавлено',
+      'Вы добавили рецепт' + formData.get('namerecept') +
       'В ближайшее время рецепт пройдет обработку и добавится на сайт',
       'success'
     )
-    console.log("Записались");
+    console.log("Успешно");
   }
   formData = new FormData();
 }
